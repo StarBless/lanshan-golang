3b3bfca5925a29607e993e0c6c74f5da5e2848a9
StarBlesszsy <3338342776@qq.com>
2022-07-24 23:46:17 +0800

    1

--- Week_02 task/task 2/2.P1443 马的遍历/2.go
+++ Week_02 task/task 2/2.P1443 马的遍历/2.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+var Map [450][450]int
+var vis [450][450]int
+var n, m, x, y int
+var dir = [][]int{
+	{2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+	{-2, -1}, {-1, -2}, {1, -2}, {2, -1}}
+
+func main() {
+	fmt.Scanf("%d %d %d %d\n", &n, &m, &x, &y)
 }
